bf7b357db3d42d0babdbf5371038ca82d3a6c963
Lærke G. Vigh <eaa25lgv@students.eaaa.dk>
2025-10-15 09:04:28 +0200

    new changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,39 @@
 "use strict"
 
+/*FORSIDE*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logo = document.querySelector(".logo");
+  const splash = document.querySelector(".splash-screen");
+
+  // logo-animation efter kort delay
+  setTimeout(() => {
+    logo.classList.add("animate");
+  }, 800); // 0.8 sekunder efter load
+
+  // fade splash-screen ud
+  setTimeout(() => {
+    splash.classList.add("fade-out");
+  }, 2500); // fade starter efter 2.5 sekunder
+
+  // skift til næste side (location.html)
+  setTimeout(() => {
+    window.location.href = "sites/location.html";
+  }, 3500); 
+});
+
+
+/*LOCATION*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const locationSection = document.querySelector(".location");
+
+  // Tilføj fade-in efter kort delay
+  setTimeout(() => {
+    locationSection.classList.add("fade-in");
+  }, 100); 
+});
+
 
 
 /*SPILGALLERI*/
